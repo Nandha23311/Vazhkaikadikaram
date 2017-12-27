e8d535388d754cc41a9c0616f267fea6f152d8f4
Your Name <you@example.com>
2017-12-27 01:41:12 -0800

    kadikaram2

--- server/route.js
+++ server/route.js
@@ -4,5 +4,6 @@ module.exports=function(router){
 router.post('/api/funapp/saveuser',saveUser.newSave);
 router.get('/api/funapp/home',saveUser.home);
 router.get('/api/funapp/login',saveUser.login)
+router.post('/api/funapp/moblogin',saveUser.login)
 router.post('/api/funapp/view',saveUser.view)
 }
