60bca9c1c79c007b8c4c4f726c5f6a3e3b405145
Nandakumar M <nandakumar.m@redbus.com>
2020-07-29 12:57:51 +0530

    routes changes

--- server/route.js
+++ server/route.js
@@ -1,13 +1,14 @@
-var saveUser=require('./controller/SaveController.js')
+var saveUser = require('./controller/SaveController.js')
 
-module.exports=function(router){
-router.post('/api/funapp/saveuser',saveUser.newSave);
-router.get('/api/funapp/home',saveUser.home);
-router.get('/api/funapp/login',saveUser.login)
-router.post('/api/funapp/moblogin',saveUser.view)
-router.post('/api/funapp/view',saveUser.view)
-  
-router.post('/s',saveUser.newSave);
-router.get('/',saveUser.home);
-router.post('/view',saveUser.view)
+module.exports = function (router) {
+    router.post('/api/funapp/saveuser', saveUser.newSave);
+    router.get('/api/funapp/home', saveUser.home);
+    router.get('/api/funapp/login', saveUser.login)
+    router.post('/api/funapp/moblogin', saveUser.view)
+    router.post('/api/funapp/view', saveUser.view)
+
+
+    router.get('/', saveUser.home);
+    router.post('/s', saveUser.newSave);
+    router.post('/view', saveUser.view)
 }
